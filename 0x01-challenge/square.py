6b125a0263ddc5e3055716c40939f61e18267919
kabadasori <38851055+kabadasori@users.noreply.github.com>
2023-10-14 15:19:56 +0300

    fix_code_challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""
-Docstring
-"""
+""" Module for square class"""
 
 
 class Square():
-
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Documentation """
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square
-            Here is the error """
+        """ Area of the square """
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """ Documentation """
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Documentation """
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = rect(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
